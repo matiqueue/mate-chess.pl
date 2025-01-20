e7edb06c87b128ac27097641468ce9db4ba6ff5c
matiqueue <matiqueue@gmail.com>
2025-01-20 16:15:42 +0100

    /home initialization

---
+++ apps/web/app/home/layout.tsx
@@ -0,0 +1,47 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs"
+import { Geist, Geist_Mono } from "next/font/google"
+
+import { Providers } from "@/components/providers"
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+        >
+          <SignedOut>
+            <SignInButton />
+
+            <SignUpButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <Providers>{children}</Providers>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
---
+++ apps/web/components/landing-page/example.tsx
@@ -0,0 +1,15 @@
+export const Blok1 = () => {
+  return (
+    <div>
+      <h1>blok1</h1>
+    </div>
+  )
+}
+
+export const Blok2 = () => {
+  return (
+    <div>
+      <h1>blok2</h1>
+    </div>
+  )
+}
