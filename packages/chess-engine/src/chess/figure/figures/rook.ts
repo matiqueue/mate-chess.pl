c86c544420707678c3b8134bdb728b7ec4c3f2a0
Maxicom0001 <karate.lynx@gmail.com>
2025-02-14 17:40:44 +0100

    - implement rook figure

--- packages/chess-engine/src/chess/figure/figures/rook.ts
+++ packages/chess-engine/src/chess/figure/figures/rook.ts
@@ -1,1 +1,61 @@
- 
+import { Figure } from "@modules/utils/figureUtils"
+import figureType from "@modules/shared/types/figureType"
+import color from "@modules/shared/types/colorType"
+import { Board, Position } from "@modules/utils/boardUtils"
+
+class Queen extends Figure {
+  constructor(type: figureType, color: color, position: Position, board: Board) {
+    super(type, color, position, board)
+  }
+
+  override isPositionValid(target: Position): boolean {
+    if (!this.isPositionExisting(target)) return false
+    const deltaX = target.x - this.position.x
+    const deltaY = target.y - this.position.y
+
+    if (deltaX !== 0 && deltaY !== 0) {
+      return false
+    }
+    return true
+  }
+
+  override isMoveValid(target: Position): boolean {
+    if (!this.isPositionValid(target)) return false
+
+    if (target.figure?.color === this.color) return false
+
+    const deltaX = target.x - this.position.x
+    const deltaY = target.y - this.position.y
+
+    if (deltaX !== 0 && deltaY !== 0) {
+      return false
+    }
+
+    const signX = deltaX === 0 ? 0 : deltaX > 0 ? 1 : -1
+    const signY = deltaY === 0 ? 0 : deltaY > 0 ? 1 : -1
+
+    let currentX = target.x - signX
+    let currentY = target.y - signY
+
+    if (currentX > 7 || currentX < 0 || currentY > 7 || currentY < 0) {
+      return false
+    }
+
+    while (currentX !== this.position.x || currentY !== this.position.y) {
+      const currentPosition = this._board.getPositionByCords(currentX, currentY)
+      if (!currentPosition) {
+        return false
+      }
+      if (currentPosition.figure) {
+        if (currentPosition.figure.type !== "king" || undefined || null) {
+          return false
+        }
+      }
+      currentX -= signX
+      currentY -= signY
+    }
+
+    return true
+  }
+}
+export default Queen
