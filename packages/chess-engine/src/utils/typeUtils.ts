e2045e3cc9875ae34d0bcf0711a993bf1b1f39a3
Maxicom0001 <karate.lynx@gmail.com>
2025-02-14 17:29:30 +0100

    - types rename to prevent misinterpreting

--- packages/chess-engine/src/utils/typeUtils.ts
+++ packages/chess-engine/src/utils/typeUtils.ts
@@ -1,5 +1,5 @@
 import Move from "@modules/shared/types/moveType"
-import Color from "@modules/shared/types/colorType"
-import Figure from "@modules/shared/types/figureType"
+import ColorTypes from "@modules/shared/types/colorType"
+import FigureTypes from "@modules/shared/types/figureType"
 
-export type { Move, Color, Figure }
+export type { Move, ColorTypes, FigureTypes }
