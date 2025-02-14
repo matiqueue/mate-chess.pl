b194d3061999177e6d9f5260a21b3592ef54c478
Maxicom0001 <karate.lynx@gmail.com>
2025-02-14 18:53:52 +0100

    - prepping for frontend connection

---
+++ packages/chess-engine/src/chessGameExtraLayer.ts
@@ -0,0 +1,19 @@
+import ChessGame from "@modules/chess/chessGame"
+
+class ChessGameExtraLayer extends ChessGame {
+  constructor() {
+    super()
+  }
+
+  protected override start() {
+    super.start()
+  }
+
+  protected override process() {
+    super.process()
+  }
+
+  protected override setupFigures() {
+    super.setupFigures()
+  }
+}
