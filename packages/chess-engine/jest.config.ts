fcf2b7f8d64d9927db65528760f9180b98bc5805
Szymon Góral <matiqueue@gmail.com>
2025-02-15 17:11:02 +0100

    jest remap

--- packages/chess-engine/jest.config.ts
+++ packages/chess-engine/jest.config.ts
@@ -8,7 +8,7 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     "^@utils/(.*)$": "<rootDir>/src/utils/$1",
-    "^@chesstypes/(.*)$": "<rootDir>/src/shared/types/$1",
     "^@modules/(.*)$": "<rootDir>/src/$1",
+    "^@shared/types/(.*)$": "<rootDir>/src/shared/types/$1",
   },
 }
