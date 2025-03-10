0ff5ab4324fa6b316a0b5cb869bb39d2e142ce5b
Szymon <matiqueue@gmail.com>
2025-03-10 20:04:55 +0100

    Add ClerkProvider to layout and wrap Providers component

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,5 @@
 // apps/web/app/layout.tsx
 import { Providers } from "@/components/providers"
-import { ClerkProvider } from "@clerk/nextjs"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -10,12 +9,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body>
-          <Providers>{children}</Providers>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
   )
 }
--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -9,6 +9,7 @@ import { I18nextProvider } from "react-i18next"
 import i18n from "@/i18n"
 import { AudioProvider } from "./home/audio-provider"
 import { ErrorContextProvider } from "@/contexts/ErrorContextProvider"
+import { ClerkProvider } from "@clerk/nextjs"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
@@ -18,13 +19,15 @@ export function Providers({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true} enableColorScheme disableTransitionOnChange>
-      <I18nextProvider i18n={i18n}>
-        <Analytics />
-        <AudioProvider>
-          <ErrorContextProvider>{children}</ErrorContextProvider>
-        </AudioProvider>
-      </I18nextProvider>
-    </ThemeProvider>
+    <ClerkProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true} enableColorScheme disableTransitionOnChange>
+        <I18nextProvider i18n={i18n}>
+          <Analytics />
+          <AudioProvider>
+            <ErrorContextProvider>{children}</ErrorContextProvider>
+          </AudioProvider>
+        </I18nextProvider>
+      </ThemeProvider>
+    </ClerkProvider>
   )
 }
