9a9ce0f29772b540397dd6ad8fbc4e1f919b2d44
Maxicom0001 <karate.lynx@gmail.com>
2025-03-18 15:48:38 +0100

    - preparing new gamemode

--- packages/chess-engine/src/ai/engine.ts
+++ packages/chess-engine/src/ai/engine.ts
@@ -1,6 +1,6 @@
 import ChessGame from "@modules/chess/chessGame.js"
-import { color, Move } from "@modules/types.js"
-import { Board, Position } from "@utils/boardUtils"
+import { color, figureType, Move } from "@modules/types.js"
+import { Board } from "@utils/boardUtils"
 import { gameStatusType } from "@shared/types/gameStatusType.js"
 import { PromotionFigureType } from "@modules/types.js"
 
@@ -17,61 +17,19 @@ class ChessAI extends ChessGame {
   }
 
   protected override async process(): Promise<void> {
-    await super.process()
-
     if (this.currentPlayer === this.aiColour) {
+      if (this.awaitingPromotion) {
+        this.promotionTo(figureType.queen)
+      }
       this.makeMove(this.determineBestMove())
     }
+    await super.process()
   }
 
   // override promotionTo: (selectedFigure: PromotionFigureType) => boolean
 
   override makeMove(move: Move): boolean {
     return super.makeMove(move)
-  }
-
-  override undoMove(): boolean {
-    return super.undoMove()
-  }
-
-  override getMoveHistory(): any {
-    return super.getMoveHistory()
-  }
-
-  override gameDraw() {
-    super.gameDraw()
-  }
-
-  override get currentPlayer(): color.White | color.Black {
-    return super.currentPlayer
-  }
-
-  override set currentPlayer(value: color.White | color.Black) {
-    super.currentPlayer = value
-  }
-
-  override get board(): Board {
-    return super.board
-  }
-
-  override get isGameOn(): boolean {
-    return super.isGameOn
-  }
-
-  override set gameStatus(value: gameStatusType) {
-    super.gameStatus = value
-  }
-
-  override get gameStatus(): gameStatusType {
-    return super.gameStatus
-  }
-
-  protected override setupFigures() {
-    super.setupFigures()
-  }
-
-  override get awaitingPromotion(): boolean {
-    return super.awaitingPromotion
   }
 
   private determineBestMove(): Move {
