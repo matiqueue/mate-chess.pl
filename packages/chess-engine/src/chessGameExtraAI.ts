847a1de9debabafbf6faa7c23ac019ec90bff63b
Maxicom0001 <karate.lynx@gmail.com>
2025-03-22 22:41:20 +0100

    - ai v1.2

--- packages/chess-engine/src/chessGameExtraAI.ts
+++ packages/chess-engine/src/chessGameExtraAI.ts
@@ -3,7 +3,7 @@ import { color } from "@shared/types/colorType"
 
 class ChessGameExtraAI extends ChessAi {
   constructor(aiColor: color) {
-    super(aiColor)
+    super(aiColor, 2)
   }
 
   public override start() {
