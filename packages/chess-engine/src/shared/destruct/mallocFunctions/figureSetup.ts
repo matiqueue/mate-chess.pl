bd1c4fd1d0bc72ecb0ea7f1ed37afaca783f4592
Maxicom0001 <karate.lynx@gmail.com>
2025-03-28 19:01:53 +0100

    - fix code generation chunk error

--- packages/chess-engine/src/shared/destruct/mallocFunctions/figureSetup.ts
+++ packages/chess-engine/src/shared/destruct/mallocFunctions/figureSetup.ts
@@ -1,4 +1,4 @@
-import ChessGame from "@modules/chess/chessGame.js"
+import ChessGame from "@modules/chess/chessGame"
 
 export const setupFiguresStandard = (game: ChessGame) => {
   game.setupFigures()
